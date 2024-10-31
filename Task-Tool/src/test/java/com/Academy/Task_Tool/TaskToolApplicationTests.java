89d6428e5c2766d2556668c6b18ecede4b2c213e
ShahdanHegazy <shahdanhegazy0@gmail.com>
2024-10-31 10:12:12 +0300

    add back folders

---
+++ Task-Tool/src/test/java/com/Academy/Task_Tool/TaskToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Academy.Task_Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
