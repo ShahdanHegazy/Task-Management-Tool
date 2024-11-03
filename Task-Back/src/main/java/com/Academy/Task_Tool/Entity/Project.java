e1826ee6dbbf1441da005170fb43f626e443b780
Mostafa-Kepesh <155603995+Mostafa-Kepesh@users.noreply.github.com>
2024-11-03 16:16:45 +0200

    creat table Entity

---
+++ Task-Back/src/main/java/com/Academy/Task_Tool/Entity/Project.java
@@ -0,0 +1,40 @@
+package com.Academy.Task_Tool.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "project")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "project_id")
+    private int project_id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_date")
+    private String start_date;
+
+    @Column(name = "end_date")
+    private String end_date;
+
+    @Column(name = "status")
+    private String status;
+
+}
