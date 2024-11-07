6c6c605eb8bee45012ddac85ff3d9ec1067145f5
Ali <alysaied461@gmail.com>
2024-11-07 13:13:53 +0200

    small edite

--- Task-Back/src/main/java/com/Academy/Task_Tool/Entity/Comment.java
+++ Task-Back/src/main/java/com/Academy/Task_Tool/Entity/Comment.java
@@ -22,17 +22,11 @@ public class Comment {
     @Column(name = "comment_id" )
     private Integer commentid;
 
-    @Column(name = "task_id" )
-    private Integer task_id;
-
     @Column(name = "creat_at" )
     private Timestamp creat_at;
 
     @Column(name = "content")
     private String content;
-
-    @Column(name = "user_id")
-    private Integer user_id;
 
     @Column(name = "update_at")
     private String update_at ;
