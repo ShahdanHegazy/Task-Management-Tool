3fa51ef978312b694140af22bb1bbe3799712bc3
ShahdanHegazy <shahdanhegazy0@gmail.com>
2024-11-11 17:42:55 +0200

    get all asigned projects + get project with tasks

--- Task-Back/src/main/java/com/Academy/Task_Tool/Controller/MemberController.java
+++ Task-Back/src/main/java/com/Academy/Task_Tool/Controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.Academy.Task_Tool.Controller;
 
+import com.Academy.Task_Tool.DTO.ProjectDto;
+import com.Academy.Task_Tool.DTO.TaskDto;
 import com.Academy.Task_Tool.Entity.Comment;
 import com.Academy.Task_Tool.Entity.Task;
 import com.Academy.Task_Tool.Services.MemberService;
@@ -15,34 +17,40 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<ProjectDto>> getAllAssignedProjects(@PathVariable int userId) {
+        return memberService.getAllAssignedProjects(userId);
+    }
     @GetMapping("/project{projectId}/alltasks")
-    public ResponseEntity<List<Task>> getAllTasks(@PathVariable Long projectId) {
-        return memberService.getAllTasks(projectId);
+    public ResponseEntity<List<TaskDto>> getProjectWithTasks(@PathVariable int projectId) {
+        return memberService.getProjectWithTasks(projectId);
     }
 
-    @PutMapping("/project{projectId}/task{taskId}")
-    public ResponseEntity<String> updateTaskStatus(@RequestBody Task task ,@PathVariable int projectId, @PathVariable int taskId) {
-        return memberService.updateTaskStatus(task ,projectId,taskId);
+    @PutMapping("/project{projectId}/taskStatus{taskId}")
+    public ResponseEntity<String> moveTask(@PathVariable int taskId , @PathVariable int sourceListId , @PathVariable int targetListId) {
+        return memberService.moveTask(taskId , sourceListId ,targetListId);
     }
 
-    @GetMapping ("/project{projectId}/task{taskId}")
-    public ResponseEntity<Task> getTask(@PathVariable int projectId, @PathVariable int taskId) {
-        return memberService.getTask(projectId,taskId);
-    }
 
-    @PostMapping("/project{projectId}/task{taskId}")
-    public ResponseEntity<Comment> createComment(@RequestBody Comment comment,@PathVariable int projectId, @PathVariable int taskId) {
-        return memberService.createComment(comment,projectId,taskId);
-    }
-
-    @PutMapping("/project{projectId}/task{taskId}/{commentId}")
-    public ResponseEntity<Comment> updateComment(@RequestBody Comment comment,@PathVariable int projectId, @PathVariable int taskId, @PathVariable int commentId) {
-        return memberService.updateComment(comment,projectId,taskId,commentId);
-    }
-
-    @DeleteMapping("/project{projectId}/task{taskId}")
-    public ResponseEntity<String> deleteComment(@PathVariable int projectId, @PathVariable int taskId,@PathVariable int commentId) {
-        return memberService.deleteComment(projectId,taskId,commentId);
-    }
+//    @GetMapping ("/project{projectId}/task{taskId}")
+//    public ResponseEntity<Task> getTask(@PathVariable int projectId, @PathVariable int taskId) {
+//        return memberService.getTask(projectId,taskId);
+//    }
+//
+//    @PostMapping("/project{projectId}/task{taskId}")
+//    public ResponseEntity<Comment> createComment(@RequestBody Comment comment,@PathVariable int projectId, @PathVariable int taskId) {
+//        return memberService.createComment(comment,projectId,taskId);
+//    }
+//
+//    @PutMapping("/project{projectId}/task{taskId}/{commentId}")
+//    public ResponseEntity<Comment> updateComment(@RequestBody Comment comment,@PathVariable int projectId, @PathVariable int taskId, @PathVariable int commentId) {
+//        return memberService.updateComment(comment,projectId,taskId,commentId);
+//    }
+//
+//    @DeleteMapping("/project{projectId}/task{taskId}")
+//    public ResponseEntity<String> deleteComment(@PathVariable int projectId, @PathVariable int taskId,@PathVariable int commentId) {
+//        return memberService.deleteComment(projectId,taskId,commentId);
+//    }
 
 }
--- Task-Back/src/main/java/com/Academy/Task_Tool/Services/MemberService.java
+++ Task-Back/src/main/java/com/Academy/Task_Tool/Services/MemberService.java
@@ -1,17 +1,18 @@
 package com.Academy.Task_Tool.Services;
 
-import com.Academy.Task_Tool.Entity.Comment;
-import com.Academy.Task_Tool.Entity.Task;
-import com.Academy.Task_Tool.Repository.CommentRepository;
-import com.Academy.Task_Tool.Repository.TaskRepository;
+import com.Academy.Task_Tool.DTO.ProjectDto;
+import com.Academy.Task_Tool.DTO.TaskDto;
+import com.Academy.Task_Tool.Entity.*;
+import com.Academy.Task_Tool.Repository.*;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class MemberService {
@@ -20,45 +21,94 @@ public class MemberService {
     TaskRepository taskRepository;
     @Autowired
     CommentRepository commentRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ProjectRepository projectRepository;
+    @Autowired
+    TaskStatusRepository taskStatusRepository;
 
-    
-    public ResponseEntity<List<Task>> getAllTasks(Long projectId) {
-        Optional<Task> optionalTask = taskRepository.findById(projectId);
+//get all assigned projects
+    public ResponseEntity<List<ProjectDto>> getAllAssignedProjects(int userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new RuntimeException("User not found")
+        );
+        Set<Project> projects = user.getAssignedProjects();
+        List<ProjectDto> projectDTOs = projects.stream()
+                .map(project -> {
+                    ProjectDto projectDTO = new ProjectDto();
+                    projectDTO.setProject_id(project.getProject_id());
+                    projectDTO.setTitle(project.getTitle());
+                    return projectDTO;
+                })
+                .collect(Collectors.toList());
 
-        if (optionalTask.isPresent()) {
-            List<Task> tasks = new ArrayList<>();
-            tasks.add(optionalTask.get());
-            return new ResponseEntity<>(tasks, HttpStatus.OK);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return new ResponseEntity<>(projectDTOs, HttpStatus.OK);
     }
 
-    public ResponseEntity<String> updateTaskStatus(Task task, int projectId, int taskId) {
-        taskRepository.findById(taskId);
-        taskRepository.save(task);
-        return new ResponseEntity<>( "Task updated successfully", HttpStatus.OK);
+
+    //get all tasks
+    public ResponseEntity<List<TaskDto>> getProjectWithTasks(int projectId) {
+        Project project = projectRepository.findById(projectId).orElseThrow(
+                ()->new RuntimeException("Project not found")
+        );
+        Set<Task> tasks = project.getAssignedTasks();
+        List<TaskDto> taskDtos = tasks.stream()
+                .map(
+                        task -> {
+                            TaskDto taskDTO = new TaskDto();
+                            taskDTO.setTaskId(task.getTaskId());
+                            taskDTO.setTitle(task.getTitle());
+                            taskDTO.setDescription(task.getDescription());
+                            taskDTO.setPriority(task.getPriority());
+                            taskDTO.setPriority(task.getPriority());
+                            taskDTO.setDueDate(task.getDueDate());
+                            taskDTO.setCreateAt(task.getCreateAt());
+                            taskDTO.setAssignedTo(task.getAssignedTo());
+                            return taskDTO;
+                        }).collect(Collectors.toList());
+
+        return new ResponseEntity<>(taskDtos, HttpStatus.OK);
     }
 
-    public ResponseEntity<Task> getTask(int projectId, int taskId) {
-         taskRepository.findById(taskId);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<String> moveTask(int taskId, int sourceListId, int targetListId) {
+        Task task = taskRepository.findById(taskId).orElseThrow(()-> new EntityNotFoundException("Task not found"));
+//        List sourceList = taskStatusRepository.findById(sourceListId).orElseThrow(()-> new EntityNotFoundException("Task status not found"));
+//        List targetList = taskStatusRepository.findById(targetListId).orElseThrow(()-> new EntityNotFoundException("Task status not found"));
+
+        return null;
     }
 
-    public ResponseEntity<Comment> createComment(Comment comment ,int projectId, int taskId) {
-        commentRepository.save(comment);
-        return new ResponseEntity<>(comment, HttpStatus.CREATED);
-    }
+    // update task status
 
-    public ResponseEntity<Comment> updateComment(Comment comment , int projectId, int taskId, int commentId) {
-        Comment updated = commentRepository.findById(commentId).orElseThrow();
-        updated.setContent(comment.getContent());
-        updated.setUpdate_at(comment.getUpdate_at());
-        return new ResponseEntity<>(updated, HttpStatus.OK);
-    }
 
-        public ResponseEntity<String> deleteComment(int projectId, int taskId , int commentId) {
-        commentRepository.deleteById(commentId);
-        return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
-    }
+
+//    //get task
+//    public ResponseEntity<Task> getTask(int projectId, int taskId) {
+//         taskRepository.findById(taskId);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    //create comment
+//    public ResponseEntity<Comment> createComment(Comment comment ,int projectId, int taskId) {
+//        commentRepository.save(comment);
+//        return new ResponseEntity<>(comment, HttpStatus.CREATED);
+//    }
+//
+//    // update comment
+//    public ResponseEntity<Comment> updateComment(Comment comment , int projectId, int taskId, int commentId) {
+//        Comment updated = commentRepository.findById(commentId).orElseThrow();
+//        updated.setContent(comment.getContent());
+//        updated.setUpdate_at(comment.getUpdate_at());
+//        return new ResponseEntity<>(updated, HttpStatus.OK);
+//    }
+//
+//    // delete comment
+//
+//        public ResponseEntity<String> deleteComment(int projectId, int taskId , int commentId) {
+//        commentRepository.deleteById(commentId);
+//        return new ResponseEntity<>("deleted successfully", HttpStatus.OK);
+//    }
+
+
 }
