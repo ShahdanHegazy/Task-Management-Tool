b3e50440b9fefe3cb525f45e1440a1309a74e9b4
ShahdanHegazy <shahdanhegazy0@gmail.com>
2024-11-28 16:34:05 +0200

    update get board response

--- Task-Back/src/main/java/com/Academy/Task_Tool/DTO/CardBoardDto.java
+++ Task-Back/src/main/java/com/Academy/Task_Tool/DTO/CardBoardDto.java
@@ -2,8 +2,17 @@ package com.Academy.Task_Tool.DTO;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 public class CardBoardDto {
     private Integer cardId;
     private String title;
+    private Timestamp dueDate;
+    private String description;
+    private String  priority;
+    private Timestamp createAt;
+    private Integer createBy;
+    private Integer assignedTo;
+
 }
